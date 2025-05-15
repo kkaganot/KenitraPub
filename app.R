a7c2fbc75d29faab340201ddd327172856a5b9f0
kkaganot <fpopescu@greenpeace.org>
2025-05-15 20:41:56 +1000

    changed from 3 files to only one: app.R

--- app.R
+++ app.R
@@ -1,4 +1,129 @@
+####################
+#Project: Kenitra Shiny 2025
 
+####################
+
+##########################################
+#Load some packages
+##########################################
+#install.packages(“openair”)
+#install.packages(“worldmet”)
+#install.packages(“dplyr”)
+#install.packages(“lubridate”)
+#install.packages("shiny")
+#install.packages("shinythemes")
+#install.packages(leaflet)
+
+library(openair)
+library(worldmet)
+library(dplyr)
+library(lubridate)
+library(shiny)
+library(shinythemes)
+
+##########################################
+#Tell R which folder to work in
+#update the file path to match where you saved your data 
+##########################################
+#setwd("/Users/Flo/Library/CloudStorage/GoogleDrive-fpopescu@greenpeace.org/My Drive/APU 2025 Florin/R Shiny/KenitraShiny")
+#setwd("/Users/Flo/Documents/R and Shiny/KenitraDeploy")
+setwd("/Users/Flo/KPub")
+#setwd("G:/Shared drives/GPEA-GPI-Clean-Air-Unit/Crew Unit Secondments/Florin Secondment 2025/APU 2025 Florin/R Shiny/KenitraShiny")
+#setwd("/Users/afarrow/Library/CloudStorage/GoogleDrive-afarrow@greenpeace.org/Shared\ drives/GPEA-GPI-Clean-Air-Unit/Crew\ Unit\ Secondments/Florin\ Secondment\ 2025/APU\ 2025\ Florin/R\ Shiny/KenitraShiny")
+##########################################
+
+#Define the EPA correction function
+##########################################
+EPAcorrection <- function(rawPM, rawRH){
+  # Ensure both rawPM and rawRH are vectors of the same length
+  result <- numeric(length(rawPM))  # To store results
+  #loop over the data
+  for (i in 1:length(rawPM)) {
+    #For raw readings below 30 µg/m³:
+    #PM2.5 = [0.524 × PAcfatm] - [0.0862 × RHraw] + 5.75
+    if(rawPM[i] < 30){
+      result[i] <- (0.524 * rawPM[i]) - (0.0862 * rawRH[i]) + 5.75
+      #For raw readings between 30 and 50 µg/m³:
+      #PM2.5 = [0.786 × (PAcfatm/20 - 3/2) + 0.524 × (1 - (PAcfatm/20 - 3/2))] x PAcfatm - [0.0862 × RHraw] + 5.75
+    } else if(rawPM[i] >= 30 & rawPM[i] <= 50){
+      result[i] <- (0.786 * (rawPM[i]/20 - 3/2) + 0.524 * (1 - (rawPM[i]/20 - 3/2))) * rawPM[i] - (0.0862 * rawRH[i]) + 5.75
+      #For raw readings between 50 and 210 µg/m³:
+      #PM2.5 = [0.786 × PAcfatm] - [0.0862 × RHraw] + 5.75
+    } else if(rawPM[i] >= 50 & rawPM[i] <= 210){
+      result[i] <- (0.786 * rawPM[i]) - (0.0862 * rawRH[i]) + 5.75
+      #For raw readings between 210 and 260 µg/m³:
+      #PM2.5 = [0.69 × (PAcfatm/50 - 21/5) + 0.786 × (1 - (PAcfatm/50 - 21/5))] x PAcfatm- [0.0862 × RHraw × (1 - (PAcfatm/50 - 21/5))] + [2.966 × (PAcfatm/50 - 21/5)] + [5.75 × (1 - (PAcfatm/50 - 21/5))] + [8.84 × (10-4) × PAcfatm2 × (PAcfatm/50 - 21/5)]
+    } else if(rawPM[i] >= 210 & rawPM[i] <= 260){
+      result[i] <- (0.69 * (rawPM[i]/50 - 21/5) + 0.786 * (1 - (rawPM[i]/50 - 21/5))) * rawPM[i] - (0.0862 * rawRH[i] * (1 - (rawPM[i]/50 - 21/5))) + (2.966 * (rawPM[i]/50 - 21/5)) + (5.75 * (1 - (rawPM[i]/50 - 21/5))) + (0.000884 * rawPM[i]^2 * (rawPM[i]/50 - 21/5))
+      #For raw readings above 260 µg/m³:
+      #PM2.5 = 2.966 + [0.69 × PAcfatm] + [8.84 × 10-4 × PAcfatm2 ]
+    } else if(rawPM[i] > 260){
+      result[i] <- 2.966 + (0.69 * rawPM[i]) + (0.000884 * rawPM[i]^2)
+    }
+  }
+  return(result)
+}
+##########################################
+#Read some data
+
+#Read the particulate matter data
+PMdata<-read.csv("new_AirGradientData.csv")
+#Read the Met Data
+wind<-read.csv("new_MeteorologicalData.csv")
+
+##########################################
+#Rename columns in both files
+#The date column must be called 'date'
+PMdata$date <- PMdata$Local.Date.Time
+wind$date <- wind$datetime
+
+#The PM2.5 column name can be shorter
+PMdata$PM25_raw<-PMdata$PM2.5..μg.m...raw
+#The CO2..ppm. column name can be shorter
+PMdata$CO2<-PMdata$CO2..ppm..raw
+
+#The wind speed and direction columns can be "ws" and "wd"
+wind$ws<-wind$windspeed
+wind$wd<-wind$winddir
+
+##########################################
+#Apply the EPA correction function to the PM2.5 data
+PMdata$PM2.5<-EPAcorrection(PMdata$PM25_raw,PMdata$Humidity.....raw)
+
+##########################################
+#Format the time zones
+PMdata$date<-as.POSIXct(strptime(PMdata$date, format='%Y-%m-%d %H:%M',tz='CET'))
+wind$date<-as.POSIXct(strptime(wind$date, format='%Y-%m-%dT%H:%M'))
+
+#########################################
+# Add the location of the 3 sensors in PMdata:
+#for Saknia 1 sensor ID 78160: 34.241828524909074, -6.555493676717981
+#for Poste dar chabab sensor ID 84563: 34.259977, -6.561911
+#for Industrielle Saknia 1 sensor ID 82925: 34.280936, -6.559629
+
+#find the IDs of the 3 sensors
+unique(PMdata[c("Location.ID", "Location.Name")])
+
+#Add a column with the latitude of the 3 sensors identified by ID
+PMdata$Location.latitude <- ifelse(PMdata$Location.ID ==78160, 34.241828524909074,
+                                   ifelse(PMdata$Location.ID == 84563, 34.259977, 34.280936))
+
+#Add a column with the longitude of the 3 sensors identified by ID
+PMdata$Location.longitude <- ifelse(PMdata$Location.ID ==78160, -6.555493676717981,
+                                    ifelse(PMdata$Location.ID == 84563, -6.561911, -6.559629))
+
+#double check coordinates, IDs and names of the 3 sensors
+unique_loc <- unique(PMdata[c("Location.ID", "Location.Name", "Location.latitude", "Location.longitude")])
+
+#############################################
+#Merge the wind and PM data
+data<-merge(PMdata,wind,by="date") 
+
+#############################################################################
+#############################################################################
+#############################################################################
+
+# This is the ui part of the script
 # Define UI
 ui <- navbarPage(
   title = "Air Quality Visualization",
@@ -13,11 +138,11 @@ ui <- navbarPage(
              through time. Simply navigate to different tabs within the app and select the data you wish to visualise. Notes on the use and interpretation 
              of each plot are provided in the 'About' tab.   
              Air quality data presented here is collected using", 
-            tags$a("AirGradient", href = "https://www.airgradient.com", target = "_blank"),
+               tags$a("AirGradient", href = "https://www.airgradient.com", target = "_blank"),
                "sensors, it has been combined with modelled wind data produced by",
-            tags$a("Visual Crossings", href = "https://www.visualcrossing.com", target = "_blank"),
+               tags$a("Visual Crossings", href = "https://www.visualcrossing.com", target = "_blank"),
                "Data analysis is performed using the R OpenAir package (Carslaw and Ropkins, 2012) and is presented using an R shiny application (Chang et al 2025)"),
-            tags$div(                                          #insert an image and center it
+             tags$div(                                          #insert an image and center it
                tags$img(src = "sky.jpeg", width = "900px"),
                style = "text-align: center;"
              )
@@ -32,9 +157,9 @@ ui <- navbarPage(
                p("Also shown is Kenitra's fossil fuel fired power plant which is one possible source of particulate
                  and gaseous air pollutants."),
              ),
-               mainPanel(
-            tags$img(src = "Map.png", width = "900", alt = "map")
-            )
+             mainPanel(
+               tags$img(src = "Map.png", width = "900", alt = "map")
+             )
            )
   ),
   
@@ -43,8 +168,8 @@ ui <- navbarPage(
            sidebarLayout(
              sidebarPanel(
                h1("Wind Rose"),
-              p("This tab shows a wind rose for your selected location and date range."),
-              hr(),
+               p("This tab shows a wind rose for your selected location and date range."),
+               hr(),
                selectInput("location_rose", "Select Location:",
                            choices = unique(data$Location.Name), 
                            selected = unique(data$Location.Name)[1]),
@@ -62,7 +187,7 @@ ui <- navbarPage(
                 The length of bars extending from the centre of the plot show how frequently winds blow from different directions.
                 Colour shading indicates the wind speed. e.g. a long bar extending to the top of the plot indicated northerly winds are frequent,
                 if most of the bar is shaded red these winds are often fast.")
-              ),
+             ),
              mainPanel(
                plotOutput("windRose")
              )
@@ -206,5 +331,172 @@ ui <- navbarPage(
            h4("References"),
            p("Carslaw, D. C. and K. Ropkins, (2012) Openair --- an R package for air quality data analysis.Environmental Modelling & Software. Volume 27-28, 52-61."),
            p("Chang W, Cheng J, Allaire J, Sievert C, Schloerke B, Xie Y, Allen J, McPherson J, Dipert A, Borges B (2025). shiny: Web Application Framework for R. R package version 1.10.0.9001, https://shiny.posit.co/."),
-           ),
+  ),
 )
+
+
+###############################
+###############################
+###############################
+
+####################
+########### Define server logic
+server <- function(input, output, session) {
+  
+  #### server logic for Wind Rose tab
+  # Reactive: Filtered data for Wind Rose tab
+  filtered_data_rose <- reactive({
+    req(input$daterange_rose)
+    subset(data,
+           Location.Name == input$location_rose &
+             date >= input$daterange_rose[1] &
+             date <= input$daterange_rose[2])
+  })
+  
+  # Render Wind Rose
+  output$windRose <- renderPlot({
+    windRose(filtered_data_rose(), pollutant = input$pollutant_rose,
+             main = paste0("Wind Rose ", input$location_rose, " ", input$pollutant_rose),
+             type = "default")
+  })
+  
+  # Download Wind Rose
+  output$downloadWindRose <- downloadHandler(
+    filename = function() {
+      paste0("Wind Rose_", input$location_rose, "_", input$pollutant_rose, ".png")
+    },
+    content = function(file) {
+      png(file, width = 800, height = 600)
+      polarPlot(filtered_data_rose(), pollutant = input$pollutant_rose, type = "default")
+      dev.off()
+    }
+  )
+  
+  #### server logic for Polar Plot tab
+  # Reactive: Filtered data for Polar Plot tab
+  filtered_data_polar <- reactive({
+    req(input$daterange_polar)
+    subset(data,
+           Location.Name == input$location_polar &
+             date >= input$daterange_polar[1] &
+             date <= input$daterange_polar[2])
+  })
+  
+  # Render Polar Plot
+  output$polarPlot <- renderPlot({
+    polarPlot(filtered_data_polar(), pollutant = input$pollutant_polar,
+              main = paste0("Polar Plot ", input$location_polar, " ", input$pollutant_polar),
+              type = "default")
+  })
+  
+  # Download Polar Plot
+  output$downloadPolarPlot <- downloadHandler(
+    filename = function() {
+      paste0("Polar_Plot_", input$location_polar, "_", input$pollutant_polar, ".png")
+    },
+    content = function(file) {
+      png(file, width = 800, height = 600)
+      polarPlot(filtered_data_polar(), pollutant = input$pollutant_polar, type = "default")
+      dev.off()
+    }
+  )
+  
+  #### server logic for Time Variation tab 
+  # Reactive: Filtered data for Time Variation tab
+  filtered_data_time <- reactive({
+    req(input$daterange_time)
+    subset(data,
+           Location.Name == input$location_time &
+             date >= input$daterange_time[1] &
+             date <= input$daterange_time[2])
+  })
+  
+  # Render Time Variation Plot
+  output$timeVariation <- renderPlot({
+    timeVariation(filtered_data_time(), pollutant = input$pollutant_time,
+                  main = paste0("Time Variation ", input$location_time, " ", input$pollutant_time),) 
+  })
+  
+  # Download Time Variation Plot
+  output$downloadTimeVariation <- downloadHandler(
+    filename = function() {
+      paste0("Time_Variation_", input$location_time, "_", input$pollutant_time, ".png")
+    },
+    content = function(file) {
+      png(file, width = 800, height = 600)
+      timeVariation(filtered_data_time(), pollutant = input$pollutant_time)
+      dev.off()
+    }
+  )
+  
+  #### server logic for Polar Frequency tab
+  # reactive - filtered data for Polar Frequency tab
+  filtered_data_freq <- reactive({
+    req(input$daterange_freq)
+    subset(data,
+           Location.Name == input$location_freq &
+             date >= input$daterange_freq[1] &
+             date <= input$daterange_freq[2] & #Aidan added next line
+             ws > 0) #Aidan removed all data with zero wind speed
+  })
+  
+  # Render Polar Freq Plot
+  output$polarFreqPlot <- renderPlot({
+    polarFreq(filtered_data_freq(), pollutant = input$pollutant_freq,
+              main = paste0("Polar Contribution Plot ", input$location_freq, " ", input$pollutant_freq),
+              statistic = "weighted.mean", ws.int = 100,offset = 100, trans = FALSE, col = "heat")
+  }) #Aidan edited the plot so that it only looks at wind direction
+  
+  # Download Polar Freq Plot
+  output$downloadPolarFreq <- downloadHandler(
+    filename = function() {
+      paste0("Polar_Contribution_", input$location_freq, "_", input$pollutant_freq, ".png")
+    },
+    content = function(file) {
+      png(file, width = 800, height = 600)
+      if (input$pollutant_freq == "None") {
+        polarFreq(filtered_data_freq())
+      } else {
+        polarFreq(filtered_data_freq(), pollutant = input$pollutant_freq)
+      }
+      dev.off()
+    }
+  )
+  
+  #### server logic for Calendar Plot tab 
+  # Reactive: Filtered data for Calendar Plot tab
+  filtered_data_calendar <- reactive({
+    req(input$daterange_calendar)
+    subset(data,
+           Location.Name == input$location_calendar &
+             date >= input$daterange_calendar[1] &
+             date <= input$daterange_calendar[2])
+  })
+  
+  # Render Calendar Plot
+  output$CalendarPlot <- renderPlot({
+    calendarPlot(filtered_data_calendar(), pollutant = input$pollutant_calendar,
+                 main = paste0("Calendar Plot ", input$location_calendar, " ", input$pollutant_calendar))
+  })
+  
+  # Download Calendar Plot
+  output$downloadCalendarPlot <- downloadHandler(
+    filename = function() {
+      paste0("Calendar_", input$location_calendar, "_", input$pollutant_calendar, ".png")
+    },
+    content = function(file) {
+      png(file, width = 800, height = 600)
+      calendarPlot(filtered_data_calendar(), pollutant = input$pollutant_calendar)
+      dev.off()
+    }
+  )
+}
+
+######################################
+######################################
+
+# Run the application
+shinyApp(ui = ui, server = server)
+
+
+
